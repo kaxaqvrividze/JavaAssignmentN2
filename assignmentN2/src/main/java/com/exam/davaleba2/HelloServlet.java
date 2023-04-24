1a02d6548ffa031ddaab87e7d3ddd797cdfae486
kaxaqvrividze <kaxa.qvrividze@gmail.com>
2023-04-24 20:09:55 +0400

    homework

--- assignmentN2/src/main/java/com/exam/davaleba2/HelloServlet.java
+++ assignmentN2/src/main/java/com/exam/davaleba2/HelloServlet.java
@@ -10,19 +10,18 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
+
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h2>" + message + "</h2>");
         out.println("</body></html>");
     }
 
-    public void destroy() {
-    }
+
 }
